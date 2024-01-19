d8f5baddabb4bf719f54960c4b375425a50d06f0
Antonio Iuliano <antonio.iuliano1993@gmail.com>
2024-01-19 10:25:02 +0100

    unit conversion, replacing catalog reference with source reference for easier access to information

--- utils/read_source_stevecat.py
+++ utils/read_source_stevecat.py
@@ -7,7 +7,7 @@ import pandas as pd
 import sys
 # E = h nu, and we want E in eV
 h_eV_Hz = constants.h / constants.value("electron volt-joule relationship") #h is 6.63e-34 J/Hz, and 1 eV = 1.60e-19 J
-
+eV2erg = constants.value("electron volt-joule relationship")/constants.erg #constants.erg is one erg in joule
 SteVeCATPath = "/home/utente/Lavoro/CTA/Catalogs/STeVeCat/STeVECat/"
 ref_table = Table.read(SteVeCATPath+"/table_spectra.csv")
 ref_table.add_index("source_name") #indexing table by fileID
@@ -16,10 +16,13 @@ def read_source(source_name):
     sourceinfo = ref_table.loc[source_name] #access all references for this source in the STeVECat
     for ifile in range(len(sourceinfo)):
         filename = "filename"
+        sourceref = "sourcereference" #NOT STEVECAT REFERENCE, which is "arXiv:2304.00835". This is the reference for the source
         if (isinstance(sourceinfo,Table)):
          filename = f"./{sourceinfo[ifile]['reference']}/{sourceinfo[ifile]['file_id']}.ecsv"
+         sourceref = sourceinfo[ifile]['reference']
         else:
          filename = f"./{sourceinfo['reference']}/{sourceinfo['file_id']}.ecsv"
+         sourceref = sourceinfo['reference']
         spectrum = Table.read(SteVeCATPath+filename)
         metaspectrum = spectrum.meta
         spectrum["start time"] = metaspectrum['mjd_start']
@@ -27,13 +30,19 @@ def read_source(source_name):
         spectrum["Catalog"] = "STEVECAT"
         spectrum["Frequency"] = spectrum["e_ref"]*1e+12/h_eV_Hz #from energy in TeV to frequency in Hz
         spectrum["Frequency"].unit = "Hz"
-        spectrum["Reference"] = "arXiv:2304.00835"
+        spectrum["Reference"] = sourceref
         spectrum["Flag"] = "Det" #assuming detection for all data, no upper limits
+        spectrum["e2dnde"] = spectrum["e_ref"] * spectrum["e_ref"] * spectrum["dnde"] * 1e+12 * eV2erg
+        spectrum["e2dnde"].unit = "erg/(cm2s)"
+        spectrum["e2dnde_errp"] = spectrum["e_ref"] * spectrum["e_ref"] * spectrum["dnde_errp"] * 1e+12 * eV2erg #assuming e_ref to be with negligible error
+        spectrum["e2dnde_errp"].unit = "erg/(cm2s)"
+        spectrum["e2dnde_errn"] = spectrum["e_ref"] * spectrum["e_ref"] * spectrum["dnde_errn"] * 1e+12 * eV2erg #assuming e_ref to be with negligible error
+        spectrum["e2dnde_errn"].unit = "erg/(cm2s)"
         outputsed = vstack([outputsed,spectrum],metadata_conflicts='silent')
         if (isinstance(sourceinfo,Table)==False):
            break
     #final processing
-    outputsed = outputsed["Frequency","dnde","dnde_errp","start time","end time","Flag","Catalog","Reference"]
+    outputsed = outputsed["Frequency","e2dnde","e2dnde_errp","start time","end time","Flag","Catalog","Reference"]
     return outputsed
 
 try:
